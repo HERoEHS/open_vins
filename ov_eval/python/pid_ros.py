125620ad4fa0081fe8d057b6bc2fb8253d775eb6
miggyo <6648kmk@gmail.com>
2025-08-01 11:17:46 +0900

    원본

--- ov_eval/python/pid_ros.py
+++ ov_eval/python/pid_ros.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 # OpenVINS: An Open Platform for Visual-Inertial Research
 # Copyright (C) 2019 Patrick Geneva
@@ -17,145 +17,141 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+
 import os
+import rosnode
+import rospy
 import sys
 import time
+
+try:
+    from xmlrpc.client import ServerProxy
+except ImportError:
+    from xmlrpclib import ServerProxy
 import psutil
-import rclpy
-from rclpy.node import Node
-
-def get_process_ros2(node_name, logger, doprint=False):
-    """
-    Finds a ROS 2 node process by its name.
-    It iterates through all system processes and checks their command line
-    to find the one matching the specified node name.
-    """
-    # Look for the __node:=<node_name> argument
-    # Node names in command line might not have a leading '/'
-    search_str = f"__node:={node_name.lstrip('/')}"
-
-    for p in psutil.process_iter(['pid', 'name', 'cmdline']):
-        try:
-            if p.info['cmdline']:
-                # Check if the node name argument is in the process's command line
-                if any(search_str in arg for arg in p.info['cmdline']):
-                    if doprint:
-                        logger.info(f"Adding new node monitor {node_name} (pid {p.info['pid']})")
-                    return p
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
-    
-    logger.warn(f"Could not find process for node {node_name}")
-    return None
 
 
-def main(args=None):
-    rclpy.init(args=args)
-    node = Node("pid_ros")
-    logger = node.get_logger()
+def get_process_ros(node_name, doprint=False):
+    # get the node object from ros
+    node_api = rosnode.get_api_uri(rospy.get_master(), node_name, skip_cache=True)[2]
+    if not node_api:
+        rospy.logwarn("could not get api of node %s (%s)" % (node_name, node_api))
+        return False
+    # now try to get the Pid of this process
+    try:
+        response = ServerProxy(node_api).getPid('/NODEINFO')
+    except:
+        rospy.logwarn("failed to get of the pid of ros node %s (%s)" % (node_name, node_api))
+        return False
+    # try to get the process using psutil
+    try:
+        process = psutil.Process(response[2])
+        if doprint:
+            rospy.loginfo("adding new node monitor %s (pid %d)" % (node_name, process.pid))
+        return process
+    except:
+        rospy.logwarn("unable to open psutil object for %s" % (response[2]))
+        return False
 
-    # Declare and get parameters
-    node.declare_parameter('nodes', '')
-    node.declare_parameter('output', '')
-    node_csv = node.get_parameter('nodes').get_parameter_value().string_value
-    save_path = node.get_parameter('output').get_parameter_value().string_value
 
-    if not node_csv or not save_path:
-        logger.error("Please specify the nodes and output file for this logger.")
-        logger.error("ros2 run ov_eval pid_ros.py --ros-args -p nodes:=<comma,separated,node,names> -p output:=<file.txt>")
+if __name__ == '__main__':
+
+    # initialize this ros node
+    rospy.init_node("pid_ros")
+
+    # check if we have our params
+    if not rospy.has_param('~nodes') or not rospy.has_param('~output'):
+        rospy.logerr("please specify the nodes and output file for this logger 1")
+        rospy.logerr("rosrun ov_eval pid_ros.py _nodes:=<comma,separated,node,names> _output:=<file.txt>")
         sys.exit(-1)
-        
+
+    # get our paramters
+    node_csv = rospy.get_param("~nodes")
     node_list = node_csv.split(',')
+    save_path = rospy.get_param("~output")
 
-    # Debug print to console
-    logger.info(f"Processes: {node_csv} ({len(node_list)} in total)")
-    logger.info(f"Save path: {save_path}")
+    # debug print to console
+    rospy.loginfo("processes: %s (%d in total)" % (node_csv, len(node_list)))
+    rospy.loginfo("save path: %s" % save_path)
 
     # ===================================================================
     # ===================================================================
 
-    # Make sure the directory is made
+    # make sure the directory is made
     if not os.path.exists(os.path.dirname(save_path)):
         try:
             os.makedirs(os.path.dirname(save_path))
         except:
-            logger.error("Unable to create the save path!")
+            rospy.logerr("unable to create the save path!!!!!")
             sys.exit(-1)
 
-    # Open the file we will write the stats into
+    # open the file we will write the stats into
     file = open(save_path, "w")
 
-    # Write header to file
+    # write header to file
     header = "# timestamp(s) summed_cpu_perc summed_mem_perc summed_threads"
-    for n in node_list:
-        get_process_ros2(n, logger, True)  # nice debug print!
-        header += f" {n}_cpu_perc {n}_mem_perc {n}_threads"
+    for node in node_list:
+        get_process_ros(node, True)  # nice debug print!
+        header += " " + str(node) + "_cpu_perc " + str(node) + "_mem_perc " + str(node) + "_threads"
     header += "\n"
     file.write(header)
 
     # ===================================================================
     # ===================================================================
 
-    # Now let's loop and get the stats for these processes
-    rate = node.create_rate(1) # 1 Hz
-    
-    ps_list = [None] * len(node_list)
-
-    try:
-        while rclpy.ok():
-            # Get the pid processes for this object
-            for i, n in enumerate(node_list):
-                if ps_list[i] is None or not ps_list[i].is_running():
-                    ps_list[i] = get_process_ros2(n, logger, False)
-            
-            # This is to initialize the cpu_percent measurement
-            for p in ps_list:
-                if p:
-                    try:
-                        p.cpu_percent(interval=None)
-                    except (psutil.NoSuchProcess, psutil.AccessDenied):
-                        continue
-            
-            # wait one second so we can collect data
-            rate.sleep()
-
-            # Loop through and get our measurement readings
-            perc_cpu = []
-            perc_mem = []
-            threads = []
-            for i in range(len(node_list)):
-                try:
-                    # Get readings
-                    p_cpu = ps_list[i].cpu_percent(interval=None)
-                    p_mem = ps_list[i].memory_percent()
-                    p_threads = ps_list[i].num_threads()
-                    # Append to our list
-                    perc_cpu.append(p_cpu)
-                    perc_mem.append(p_mem)
-                    threads.append(p_threads)
-                except (AttributeError, psutil.NoSuchProcess, psutil.AccessDenied):
-                    # Record just zeros if we do not have this value
-                    perc_cpu.append(0)
-                    perc_mem.append(0)
-                    threads.append(0)
-
-            # Print what the total summed value is
-            logger.info(f"cpu% = {sum(perc_cpu):.3f} | mem% = {sum(perc_mem):.3f} | threads = {sum(threads)}")
-
-            # Save the current stats to file!
-            data = f"{time.time():.8f} {sum(perc_cpu):.3f} {sum(perc_mem):.3f} {sum(threads)}"
-            for i in range(len(node_list)):
-                data += f" {perc_cpu[i]:.3f} {perc_mem[i]:.3f} {threads[i]}"
-            data += "\n"
-            file.write(data)
-            file.flush()
-    except KeyboardInterrupt:
-        logger.info("Shutting down pid_ros.")
-    finally:
-        # Finally close the file and shutdown!
+    # exit if we should end
+    if rospy.is_shutdown():
         file.close()
-        node.destroy_node()
-        rclpy.shutdown()
+        sys.exit(-1)
 
-if __name__ == '__main__':
-    main()
+    # now lets loop and get the stats for these processes
+    rate = rospy.Rate(1)
+    while not rospy.is_shutdown():
+
+        # get the pid processes for this object
+        ps_list = []
+        for node in node_list:
+            ps_list.append(get_process_ros(node, False))
+            try:
+                perc_cpu = ps_list[len(ps_list) - 1].cpu_percent(interval=None)
+                perc_mem = ps_list[len(ps_list) - 1].memory_percent()
+                threads = ps_list[len(ps_list) - 1].num_threads()
+            except:
+                continue
+
+        # wait one second so we can collect data
+        rate.sleep()
+
+        # loop through and get our measurement readings
+        perc_cpu = []
+        perc_mem = []
+        threads = []
+        for i in range(0, len(node_list)):
+            try:
+                # get readings
+                p_cpu = ps_list[i].cpu_percent(interval=None)
+                p_mem = ps_list[i].memory_percent()
+                p_threads = ps_list[i].num_threads()
+                # append to our list
+                perc_cpu.append(p_cpu)
+                perc_mem.append(p_mem)
+                threads.append(p_threads)
+            except:
+                # record just zeros if we do not have this value
+                perc_cpu.append(0)
+                perc_mem.append(0)
+                threads.append(0)
+
+        # print what the total summed value is
+        rospy.loginfo("cpu%% = %.3f | mem%% = %.3f | threads = %d" % (sum(perc_cpu), sum(perc_mem), sum(threads)))
+
+        # save the current stats to file!
+        data = "%.8f %.3f %.3f %d" % (time.time(), sum(perc_cpu), sum(perc_mem), sum(threads))
+        for i in range(0, len(node_list)):
+            data += " %.3f %.3f %d" % (perc_cpu[i], perc_mem[i], threads[i])
+        data += "\n"
+        file.write(data)
+        file.flush()
+
+    # finally close the file!
+    file.close()
